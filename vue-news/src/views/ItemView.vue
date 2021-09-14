e0d8be61e743c21e9c22b53e818c82ff9421b2f9
MrFabulous00 <dudrbs10400@gmail.com>
2021-09-15 01:07:18 +0900

    transition 적용

--- vue-news/src/views/ItemView.vue
+++ vue-news/src/views/ItemView.vue
@@ -19,6 +19,7 @@
     </section>
     <section>
       <!-- 질문 댓글 -->
+      <div v-html="fetchedItem.content"></div>
     </section>
     <!--{{ this.$store.state.item }}-->
     <!--<p>{{ fetchedItem.title }}</p>
@@ -45,11 +46,15 @@ export default {
 .user-container {
   display: flex;
   align-items: center;
+  padding: 0.5rem;
 }
 .fa-user {
   font-size: 2.5rem;
 }
 .user-description {
   padding-left: 8px;
+}
+.time {
+  font-size: 0.7rem;
 }
 </style>
